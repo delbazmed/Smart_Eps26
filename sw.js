a48013e4967bd0882dbf5d0abb3405cac29994b3
Delbaz_med <timsportmed@gmail.com>
2026-05-24 00:19:22 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Smart EPS Service Worker - Network First Strategy
-const CACHE = 'smart-eps-v4';
+const CACHE = 'smart-eps-v5';
 
 self.addEventListener('install', e => {
   self.skipWaiting(); // activate immediately
